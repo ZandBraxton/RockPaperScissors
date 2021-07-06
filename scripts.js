80a7472d26277cd73ec67f6c92d3c61dd9335abd
ZandBraxton <Zand.Braxton@gmail.com>
2021-07-06 12:53:48 -0500

    Accepts prompt

--- scripts.js
+++ scripts.js
@@ -1,8 +1,25 @@
 // Create variable for rock
+let rock = "Rock"
 // Create variable for scissors
+let scissors = "Scissors"
 // Create variable for paper
-// Create AI to randomly play one of the three variables
+let paper = "Paper"
 // Ask user for prompt for their choice
+let playerChoice = prompt("Rock, Paper, or Scissors?").toUpperCase();
+// Check to make sure player inputs RPS
+if (!(playerChoice == "ROCK" || playerChoice == "PAPER" || playerChoice == "SCISSORS")) {
+    alert("Invalid, please enter Rock, Paper, or Scissors")
+}
+// Convert RPS to int, make it case-insensitive
+
+
+
+
+
+console.log(playerChoice);
+
+// Create AI to randomly play one of the three variables
+
 // Calculate who won
 // If won, alert the player that they won
 // else, alert the player that they lost
