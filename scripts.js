1288f3846c5a99c4c7af1737617073c8cbfe2150
ZandBraxton <Zand.Braxton@gmail.com>
2021-07-06 12:19:07 -0500

    Improved psuedocode

--- scripts.js
+++ scripts.js
@@ -2,7 +2,8 @@
 // Create variable for scissors
 // Create variable for paper
 // Create AI to randomly play one of the three variables
-// Play one round of RPS
+// Ask user for prompt for their choice
+// Calculate who won
 // If won, alert the player that they won
 // else, alert the player that they lost
 // Make a function that loops the game through five rounds
