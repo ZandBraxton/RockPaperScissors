f79d5aeae0fe59e85f8e2ba17fc19bcb6f216e6d
ZandBraxton <Zand.Braxton@gmail.com>
2021-07-06 12:18:14 -0500

    Created psuedocode

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Create variable for rock
+// Create variable for scissors
+// Create variable for paper
+// Create AI to randomly play one of the three variables
+// Play one round of RPS
+// If won, alert the player that they won
+// else, alert the player that they lost
+// Make a function that loops the game through five rounds
+// Keep track of the score
