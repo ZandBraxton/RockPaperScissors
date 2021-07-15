29e149532e86e1f627b74da03b7b2fb38f98c0e2
ZandBraxton <Zand.Braxton@gmail.com>
2021-07-15 14:08:54 -0500

    Finalized, added results log and rules, made buttons more dynamic and added play again  function

--- scripts.js
+++ scripts.js
@@ -1,43 +1,64 @@
-
-
-
-
+//Initialize
 let p1ScoreVar = 0;
 let cpuScoreVar = 0;
-
 const p1Score = document.querySelector('#p1Score')
 const cpuScore = document.querySelector('#cpuScore')
+const log = document.querySelector('#log')
+log.textContent = "Make a selection to play!"
+rules.textContent = "First to 5 wins!"
+gameStart()
 score()
+
+
+
+function restart () {
+    p1ScoreVar = 0;
+    cpuScoreVar = 0;
+    log.textContent = "Make a selection to play!"
+}
 
 function score() {
     p1Score.textContent = `Player 1: ${p1ScoreVar}`
     cpuScore.textContent= `Computer: ${cpuScoreVar}`
 }
 
-const btnArray = document.querySelectorAll('button');
-btnArray.forEach((button) => {
-    button.addEventListener('click', () => {
-        const playerSelection = button.id.toUpperCase()
-        const computerSelection = computerPlay()
-        let result = playRound(playerSelection, computerSelection);
-        //Update score depending on result
-        switch(result) {
-            case "Tie":
+function gameStart () {
+    const btnArray = document.querySelectorAll('button');
+    for (let i = 0; i <= btnArray.length - 1; i++) {
+        switch (i) {
+            case 0:
+                btnArray[i].textContent = "Rock";
                 break;
-            case "Lose":
-                cpuScoreVar += 1;
-                score()
+            case 1:
+                btnArray[i].textContent = "Paper";
                 break;
-            case "Win":
-                p1ScoreVar += 1;
-                score()
+            case 2:
+                btnArray[i].textContent = "Scissors";
                 break;
-                
         }
+    }
+    btnArray.forEach((button) => {
+        button.addEventListener('click', () => {
+            const playerSelection = button.id.toUpperCase()
+            const computerSelection = computerPlay()
+            let result = playRound(playerSelection, computerSelection);
+            //Update score depending on result
+            switch(result) {
+                case "Tie":
+                    break;
+                case "Lose":
+                    cpuScoreVar += 1;
+                    score()
+                    break;
+                case "Win":
+                    p1ScoreVar += 1;
+                    score()
+                    break;     
+            }
+            checkResults(p1ScoreVar, cpuScoreVar)
+        });
     });
-});
-
-
+}
 
 // Create AI to randomly play one of the three variables
 function computerPlay() {
@@ -63,60 +84,55 @@ function randomNumber(min, max) {
 // Calculate who won and alert player result
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
-        alert("Tie game!")
+        log.textContent = 'Tie Game!'
         return "Tie";
     } else if (playerSelection == "ROCK") {
         if (computerSelection == "PAPER") {
-            alert("You Lose! Paper beats Rock")
+            log.textContent = "You Lose! Paper beats Rock"
             return "Lose"
         } else {
-            alert("You Win! Rock beats Scissors")
+            log.textContent = "You Win! Rock beats Scissors"
             return "Win"
         }
     } else if (playerSelection == "PAPER") {
         if (computerSelection == "SCISSORS") {
-            alert("You Lose! Scissors beats Paper")
+            log.textContent = "You Lose! Scissors beats Paper"
             return "Lose"
         } else {
-            alert("You Win! Paper beats Rock")
+            log.textContent = "You Win! Paper beats Rock"
             return "Win"
         }
     } else {
         if (computerSelection == "ROCK") {
-            alert("You Lose! Rock beats Scissors")
+            log.textContent = "You Lose! Rock beats Scissors"
             return "Lose"
         } else {
-            alert("You win! Scissors beats Paper")
+            log.textContent = "You win! Scissors beats Paper"
             return "Win"
         }
     }
 }
 
+function checkResults (p1ScoreVar, cpuScoreVar) {
+    if (p1ScoreVar === 5) {
+        log.textContent = "Congratulations! You win!"
+        playAgain()
+    } else if (cpuScoreVar === 5) {
+        log.textContent = "Game Over"
+        playAgain()
+    }
+}
 
-
-
-// // Make a function that loops the game through five rounds
-// function game() {
-//     //initialize score
-//     let p1Score = 0;
-//     let cpuScore = 0;
-//     //loop game 5 times
-//     for (let i = 0; i < 5; i++) {
-//         //Generate variables each round
-//         const playerSelection = playerPlay();
-//         const computerSelection = computerPlay();
-//         let result = playRound(playerSelection, computerSelection);
-//         //Update score depending on result
-//         switch(result) {
-//             case "Tie":
-//                 break;
-//             case "Lose":
-//                 ++cpuScore;
-//                 break;
-//             case "Win":
-//                 ++p1Score;
-//                 break;
-//         }
-//         alert(`Score: Player: ${p1Score}, Computer: ${cpuScore}`)
-//     }
-// }
+function playAgain() {
+    const btnArray = document.querySelectorAll('button');
+    for (let i = 0; i <= btnArray.length - 1; i++) {
+        btnArray[i].textContent = "Play Again"
+    }
+    btnArray.forEach((button) => {
+    button.addEventListener('click', () => {
+        gameStart()
+        restart()
+        score()
+        });
+    });
+}
